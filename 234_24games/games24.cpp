c88e509222def2bf37c9dd097bcb6c90ed409be1
KECHUAGN YOU <1453137592@qq.com>
2019-09-18 21:17:36 +0800

    Initial commit

---
+++ 234_24games/games24.cpp
@@ -0,0 +1,17 @@
+//
+// Created by youkechaung on 2019/9/18.
+//
+
+#include "games24.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sum = 0, value = 0;
+    while (cin >> value)
+    {
+        sum += value;
+    }
+    cout << sum<<endl;
+    return 0;
+}
---
+++ 234_24games/games24.h
@@ -0,0 +1,14 @@
+//
+// Created by youkechaung on 2019/9/18.
+//
+
+#ifndef CPLUSCODE_GAMES24_H
+#define CPLUSCODE_GAMES24_H
+
+
+class games24 {
+
+};
+
+
+#endif //CPLUSCODE_GAMES24_H
